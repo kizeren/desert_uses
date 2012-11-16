40a0bf5ba9521c0f6f18cafa235d270cd77cb57e
menche <menche_mt@yahoo.com>
2012-11-15 16:48:30 -0800

    Fix crash
    menche

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 -- desert_stone mod by Menche
 -- Adds the uses of normal stone to desert stone
--- License: CC-BY: Do anything you want with this code, but link back to the forum topic and/or the gitorious page.
+-- License: CC0
 
 -- Node definitions --------------------
 -- Desert Cobble
-minetest.register_node("desert_stone:desert_cobble", {
+minetest.register_node("desert_uses:desert_cobble", {
 	description = "Desert Cobblestone",
 	tiles = {"desert_stone_desert_cobble.png"},
 	is_ground_content = true,
@@ -18,13 +18,13 @@ minetest.register_node(":default:desert_stone", {
 	tiles = {"default_desert_stone.png"},
 	is_ground_content = true,
 	groups = {cracky=3},
-	drop = "desert_stone:desert_cobble",
+	drop = "desert_uses:desert_cobble",
 	sounds = default.node_sound_stone_defaults(),
 })
 
 -- Tool definitions -------------------
 -- Desert stone pickaxe
-minetest.register_tool("desert_stone:pick_desert_stone", {
+minetest.register_tool("desert_uses:pick_desert_stone", {
 	description = "Desert Stone Pickaxe",
 	inventory_image = "desert_stone_tool_desert_stonepick.png",
 	tool_capabilities = {
@@ -36,9 +36,9 @@ minetest.register_tool("desert_stone:pick_desert_stone", {
 })
 
 -- Desert stone shovel
-minetest.register_tool("desert_stone:shovel_desert_stone", {
+minetest.register_tool("desert_uses:shovel_desert_stone", {
 	description = "Desert Stone Shovel",
-	inventory_image = "desert_stone_tool_desert_stoneshovel.png",
+	inventory_image = "desert_uses_tool_desert_stoneshovel.png",
 	tool_capabilities = {
 		max_drop_level=0,
 		groupcaps={
@@ -48,9 +48,9 @@ minetest.register_tool("desert_stone:shovel_desert_stone", {
 })
 
 -- Desert stone axe
-minetest.register_tool("desert_stone:axe_desert_stone", {
+minetest.register_tool("desert_uses:axe_desert_stone", {
 	description = "Desert Stone Axe",
-	inventory_image = "desert_stone_tool_desert_stoneaxe.png",
+	inventory_image = "desert_uses_tool_desert_stoneaxe.png",
 	tool_capabilities = {
 		max_drop_level=0,
 		groupcaps={
@@ -61,9 +61,9 @@ minetest.register_tool("desert_stone:axe_desert_stone", {
 })
 
 -- Desert stone sword
-minetest.register_tool("desert_stone:sword_desert_stone", {
+minetest.register_tool("desert_uses:sword_desert_stone", {
 	description = "Desert Stone Sword",
-	inventory_image = "desert_stone_tool_desert_stonesword.png",
+	inventory_image = "desert_uses_tool_desert_stonesword.png",
 	tool_capabilities = {
 		full_punch_interval = 1.0,
 		max_drop_level=0,
@@ -78,9 +78,9 @@ minetest.register_tool("desert_stone:sword_desert_stone", {
 -- Craft definitions -------------------
 -- Desert stone pickaxe
 minetest.register_craft({
-	output = "desert_stone:pick_desert_stone",
+	output = "desert_uses:pick_desert_stone",
 	recipe = {
-		{"desert_stone:desert_cobble", "desert_stone:desert_cobble", "desert_stone:desert_cobble"},
+		{"desert_uses:desert_cobble", "desert_uses:desert_cobble", "desert_uses:desert_cobble"},
 		{"", "default:stick", ""},
 		{"", "default:stick", ""},
 	}
@@ -88,9 +88,9 @@ minetest.register_craft({
 
 -- Desert stone shovel
 minetest.register_craft({
-	output = "desert_stone:shovel_desert_stone",
+	output = "desert_uses:shovel_desert_stone",
 	recipe = {
-		{"desert_stone:desert_cobble"},
+		{"desert_uses:desert_cobble"},
 		{"default:stick"},
 		{"default:stick"},
 	}
@@ -98,30 +98,30 @@ minetest.register_craft({
 
 -- Desert stone axe
 minetest.register_craft({
-	output = "desert_stone:axe_desert_stone",
+	output = "desert_uses:axe_desert_stone",
 	recipe = {
-		{"desert_stone:desert_cobble", "desert_stone:desert_cobble"},
-		{"desert_stone:desert_cobble", "default:stick"},
+		{"desert_uses:desert_cobble", "desert_uses:desert_cobble"},
+		{"desert_uses:desert_cobble", "default:stick"},
 		{"", "default:stick"},
 	}
 })
 
 -- Desert stone axe (flipped recipe)
 minetest.register_craft({
-	output = "desert_stone:axe_desert_stone",
+	output = "desert_uses:axe_desert_stone",
 	recipe = {
-		{"desert_stone:desert_cobble", "desert_stone:desert_cobble"},
-		{"default:stick", "desert_stone:desert_cobble"},
+		{"desert_uses:desert_cobble", "desert_uses:desert_cobble"},
+		{"default:stick", "desert_uses:desert_cobble"},
 		{"default:stick", ""},
 	}
 })
 
 -- Desert stone sword
 minetest.register_craft({
-	output = "desert_stone:sword_desert_stone",
+	output = "desert_uses:sword_desert_stone",
 	recipe = {
-		{"desert_stone:desert_cobble"},
-		{"desert_stone:desert_cobble"},
+		{"desert_uses:desert_cobble"},
+		{"desert_uses:desert_cobble"},
 		{"default:stick"},
 	}
 })
@@ -138,15 +138,15 @@ minetest.register_craft({
 minetest.register_craft({
 	type = "cooking",
 	output = "default:desert_stone",
-	recipe = "desert_stone:desert_cobble",
+	recipe = "desert_uses:desert_cobble",
 })
 
 -- Furnace
 minetest.register_craft({
 	output = "default:furnace",
 	recipe = {
-		{"desert_stone:desert_cobble", "desert_stone:desert_cobble", "desert_stone:desert_cobble"},
-		{"desert_stone:desert_cobble", "", "desert_stone:desert_cobble"},
-		{"desert_stone:desert_cobble", "desert_stone:desert_cobble", "desert_stone:desert_cobble"},
+		{"desert_uses:desert_cobble", "desert_uses:desert_cobble", "desert_uses:desert_cobble"},
+		{"desert_uses:desert_cobble", "", "desert_uses:desert_cobble"},
+		{"desert_uses:desert_cobble", "desert_uses:desert_cobble", "desert_uses:desert_cobble"},
 	}
 })
